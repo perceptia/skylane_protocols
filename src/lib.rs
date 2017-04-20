5a5acec31784b6d9b6ecb466eaddda3ff1e6af50
Wojciech Kluczka <wojciech.kluczka@gmail.com>
2017-04-20 20:33:44 +0200

    Adapt to changes in `skylane` and implement client part

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2016 The Perceptia Project Developers
+// Copyright 2016-2017 The Perceptia Project Developers
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 // and associated documentation files (the "Software"), to deal in the Software without
@@ -21,20 +21,17 @@
 //! This crate is planed to provide implementation for all (stable and unstable) protocols. If
 //! something is missing, let us know.
 //!
-//! ## Server
+//! ## Implementation
 //!
 //! Each protocol description file contains requests (from client to server) and events (from
 //! server to client). In server part requests are translated to:
 //! - interfaces - traits describing methods of a Wayland protocol object
-//! - dispatches - structures translating socket data to calls to methods on objects implementing
+//! - dispatchers - structures translating socket data to calls to methods on objects implementing
 //!   appropriate interface.
+//! while events to stand-alone functions. On client side vice-versa.
 //!
 //! `Handler` structure helps bind `Dispatcher` with implementation of its `Interface` to
-//! register it in `server::Client` from `skylane` crate.
-//!
-//! ## Client
-//!
-//! TODO: Implement also client bindings.
+//! register it in `server::Connection` from `skylane` crate.
 //!
 //! ## Server examples
 //!
@@ -44,31 +41,32 @@
 //!
 //! TODO: Add examples for client.
 
+// TODO: Add comments to protocols.
+// #![warn(missing_docs)]
+
 extern crate skylane;
 extern crate byteorder;
 
 // -------------------------------------------------------------------------------------------------
 
-pub mod server {
+mod private {
     use std::io::Cursor;
-    use skylane::common::{Header, SkylaneError};
-    use skylane::server::{ClientSocket, Object, Task};
+    use skylane::server::{Bundle, Header, Object, SkylaneError, Task};
 
     /// This trait is implemented by `Dispatcher`s in protocol definitions generated from XML
-    /// files.  Every objects defined in protocol has its own `Dispatcher` which takes buffer
+    /// files. Every object defined in protocol has its own `Dispatcher` which takes buffer
     /// data, parses it and calls appropriate method of given object which implements attached
     /// interface.
     pub trait Dispatcher<I> {
         fn new() -> Self;
         fn dispatch(&mut self,
                     object: &mut I,
-                    socket: &mut ClientSocket,
+                    bundle: &mut Bundle,
                     header: &Header,
                     bytes_buf: &mut Cursor<&[u8]>,
                     fds_buf: &mut Cursor<&[u8]>)
                     -> Result<Task, SkylaneError>;
     }
-
 
     /// Binds `Dispatcher` with object implementing `Interface` trait from protocol definition.
     /// Only objects implementing `Interfaces` corresponding to `Dispatcher` can be bound
@@ -95,14 +93,22 @@ pub mod server {
         where D: Dispatcher<I>
     {
         fn dispatch(&mut self,
-                    socket: &mut ClientSocket,
+                    bundle: &mut Bundle,
                     header: &Header,
                     bytes_buf: &mut Cursor<&[u8]>,
                     fds_buf: &mut Cursor<&[u8]>)
                     -> Result<Task, SkylaneError> {
-            self.dispatcher.dispatch(&mut self.object, socket, header, bytes_buf, fds_buf)
+            self.dispatcher.dispatch(&mut self.object, bundle, header, bytes_buf, fds_buf)
         }
     }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+pub mod server {
+    use skylane::server::{Bundle, Header, ObjectId, SkylaneError, Socket, Task};
+    use private::Dispatcher;
+    pub use private::Handler;
 
     pub mod wayland {
         include!(concat!(env!("OUT_DIR"), "/wayland_server.rs"));
@@ -115,6 +121,10 @@ pub mod server {
 // -------------------------------------------------------------------------------------------------
 
 pub mod client {
+    use skylane::server::{Bundle, Header, ObjectId, SkylaneError, Socket, Task};
+    use private::Dispatcher;
+    pub use private::Handler;
+
     pub mod wayland {
         include!(concat!(env!("OUT_DIR"), "/wayland_client.rs"));
     }
